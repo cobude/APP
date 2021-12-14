cf135cbe430c104baa3dba60773c65b6aeaa29f3
cobdue <1448391978@qq.com>
2021-12-15 02:46:45 +0800

    environment finished

--- game/views.py
+++ game/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    line1 = '<h1 style="text-align:center">橘子大作战</h1>'
+    line2 = '<img src="http://p7.itc.cn/q_70/images03/20201204/ccbe9c0452db451cb505ae2b27ef10d8.jpeg" width=2000>'
+    return HttpResponse(line1+line2)
